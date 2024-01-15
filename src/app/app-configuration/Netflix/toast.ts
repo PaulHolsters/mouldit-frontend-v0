fda876532eeca4d9020a2dcf094820e7004c9f54
Paul Holsters <paul.holsters@hotmail.com>
2024-01-15 14:48:02 +0100

    toast: templating

--- src/app/app-configuration/Netflix/toast.ts
+++ src/app/app-configuration/Netflix/toast.ts
@@ -1,3 +1,8 @@
 import {Toast} from "../../components/toast/Toast";
+import {Button} from "../../components/button/Button";
 
 export const toast = new Toast('toast')
+const btn = new Button('toast-content')
+btn.structural.smartphone.setLabel('inhoud van deze toast')
+toast.contentInjection.smartphone.setTemplateComponent(btn)
+// todo krijg ik de inhoud van de toast in mijn component?
